66d57cae78ca5c3cb8c56bfbdb7145b65ed2be22
LunaSol <nhelwig99@gmail.com>
2024-02-13 22:02:32 +0100

    Made The Lunar Pickaxe change mine speed based on time of day.

--- src/main/java/dev/bedcrab/lunamod/LunaMod.java
+++ src/main/java/dev/bedcrab/lunamod/LunaMod.java
@@ -2,7 +2,12 @@ package dev.bedcrab.lunamod;
 
 import dev.bedcrab.lunamod.block.ModBlocks;
 import dev.bedcrab.lunamod.item.ModItems;
+import dev.bedcrab.lunamod.item.custom.tools.MoonPickaxeItem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +18,17 @@ public class LunaMod implements ModInitializer {
     public void onInitialize() {
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
+        ServerTickEvents.START_WORLD_TICK.register(this::onTickStart);
+    }
+
+    private void onTickStart(ServerWorld world) {
+        world.getPlayers().forEach(player -> {
+            for(ItemStack stack : player.getInventory().main) {
+                if(stack.getItem() instanceof MoonPickaxeItem) {
+                    NbtCompound tag = stack.getOrCreateNbt();
+                    tag.putBoolean("night", world.isNight());
+                }
+            }
+        });
     }
 }
--- src/main/java/dev/bedcrab/lunamod/item/custom/tools/MoonPickaxeItem.java
+++ src/main/java/dev/bedcrab/lunamod/item/custom/tools/MoonPickaxeItem.java
@@ -1,14 +1,24 @@
 package dev.bedcrab.lunamod.item.custom.tools;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterial;
-import net.minecraft.world.World;
+import net.minecraft.nbt.NbtCompound;
 
 public class MoonPickaxeItem extends PickaxeItem {
     public MoonPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
     }
-    private void registerTickEvent() {
 
+    @Override
+    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+        if(stack.hasNbt()) {
+            NbtCompound tag = stack.getNbt();
+            if(tag!=null && tag.getBoolean("night")) {
+                return MoonToolMaterial.NIGHT_MINING_SPEED;
+            }
+        }
+        return MoonToolMaterial.DAY_MINING_SPEED;
     }
 }
